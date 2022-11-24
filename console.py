5636b3e60a274a593f250e58b07d45e31a5ee6ff
Piero2023 <estibensmax2022@gmail.com>
2022-11-24 11:38:31 -0500

    add new comment

--- console.py
+++ console.py
@@ -11,7 +11,7 @@ from models.city import City
 from models.amenity import Amenity
 from models.review import Review
 
-
+#hola
 class HBNBCommand(cmd.Cmd):
     """ Contains the functionality for the HBNB console"""
 
