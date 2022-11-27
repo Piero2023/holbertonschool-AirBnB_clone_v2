a515e8db31057f3387f727c83e7e865beab5be8a
Drixner <drixnerrafael1@gmail.com>
2022-11-27 18:54:34 -0500

    Upgrade class city

--- models/city.py
+++ models/city.py
@@ -1,9 +1,19 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
+from models import storage_type
+from sqlalchemy.orm import relationship
 
 
 class City(BaseModel):
     """ The city class, contains state ID and name """
-    state_id = ""
-    name = ""
+
+    if storage_type == 'db':
+        __tablename__ = 'cities'
+        name = Column(String(128), nullable=False)
+        state_id = Column(String(60), ForeignKey('state.id'), nullable=False)
+        place = relationship("Place", backref="cities")
+    else:
+        state_id = ""
+        name = ""
