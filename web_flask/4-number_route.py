e4ee64219795538512863e237ed171d5fe21017f
Piero2023 <estibensmax2022@gmail.com>
2022-12-03 22:55:15 -0500

    A script that starts a Flask web application

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""
+Script to start flask web application
+"""
+from flask import Flask, abort
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_world():
+    """
+    return message
+    """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hello_HBNB():
+    """
+    return message
+    """
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def varible_text(text=""):
+    """
+    return message
+    """
+    text = text.replace("_", " ")
+    return "C {}".format(text)
+
+
+@app.route("/python/<text>", strict_slashes=False)
+def python_text(text="is cool"):
+    """
+    return message
+    """
+    text = text.replace("_", " ")
+    return "Python {}".format(text)
+
+
+@app.route("/number/<n>", strict_slashes=False)
+def number_n(n):
+    """
+    return message
+    """
+    if not n.isnumeric():
+        abort(404)
+    return "{} is a number".format(n)
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
